0c2a7b9e249988f10861c16c6f4fd97f15767691
WarrorKing <viktor.jose.07@gmail.com>
2024-07-28 14:07:12 -0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,37 @@
+import discord
+from bot_logic import gen_pass
+import random
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable cliente y transferirle los privilegios
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+client.run("MTI2NDY2MzQ3NTU5NTExNjU0NQ.GkYyKb.DfYxpjJCpwmSIcL7IdlS-8S-whZtIZd9LNi-zI")
